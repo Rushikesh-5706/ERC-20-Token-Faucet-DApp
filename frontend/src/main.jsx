76e4d5aa92c4e514e525c60347d909413f9207a4
Rushikesh <rushikesh@Rushikeshs-MacBook-Pro.local>
2026-02-22 09:39:12 +0530

    fix main.jsx import order for eval.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
+import "./utils/eval";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./utils/eval";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
